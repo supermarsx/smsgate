fef39195a51229529fd8702bdfbf9debd969af98
supermarsx <github_618a@imariana.com>
2025-12-27 15:57:17 +0000

    Enable StrictMode in debug builds
    Adds a method to activate Android StrictMode policies when the app is running in debug mode. This helps catch accidental disk or network access on the main thread and other potential issues during development.

--- smsrelay3/android/app/src/main/java/com/smsrelay3/SmsRelayApp.kt
+++ smsrelay3/android/app/src/main/java/com/smsrelay3/SmsRelayApp.kt
@@ -1,11 +1,31 @@
 package com.smsrelay3
 
 import android.app.Application
+import android.content.pm.ApplicationInfo
+import android.os.StrictMode
 import com.smsrelay3.util.ThemeManager
 
 class SmsRelayApp : Application() {
     override fun onCreate() {
         super.onCreate()
+        enableStrictModeForDebug()
         ThemeManager.applyMode(this)
+    }
+
+    private fun enableStrictModeForDebug() {
+        val isDebuggable = (applicationInfo.flags and ApplicationInfo.FLAG_DEBUGGABLE) != 0
+        if (!isDebuggable) return
+        StrictMode.setThreadPolicy(
+            StrictMode.ThreadPolicy.Builder()
+                .detectAll()
+                .penaltyLog()
+                .build()
+        )
+        StrictMode.setVmPolicy(
+            StrictMode.VmPolicy.Builder()
+                .detectAll()
+                .penaltyLog()
+                .build()
+        )
     }
 }
